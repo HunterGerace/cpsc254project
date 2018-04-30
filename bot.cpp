919aaabc4f8b7266542e9f054c7e26e3269ca55c
Raymond Gonzalez <rayraygking@csu.fullerton.edu>
2018-04-30 12:19:58 -0700

    finished tictactoe and made bot better

--- bot.cpp
+++ bot.cpp
@@ -7,16 +7,17 @@ int main() {
 	
 	AI bot;
 	bool exit=false;
-	string name;
-	string command;
+	string name,command;
 	//start the program, create the object
 	cout << "Greetings, user! My name is DEATHBOT, and I will be your personal bot!" << endl;
 	//greet user, input text
 	while(exit != true) {
+		cout <<"\033[1J";
+		cout<<"\033[1;1H";
 		cout << "Please type a command:" << endl;
 		cin >> command;
 		cout << endl;
-		if(command=="EXIT")
+		if(command=="EXIT"||command=="Exit"||command=="exit")
 			exit=true;
 		else
 		bot.response(command);
